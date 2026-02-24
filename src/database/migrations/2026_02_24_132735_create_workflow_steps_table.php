008b7a01a7c6eea03126fa46d9de6084ef4f29d5
luuhainam <luuhainam.it@gmail.com>
2026-02-24 20:37:45 +0700

    feat migrations

---
+++ src/database/migrations/2026_02_24_132735_create_workflow_steps_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workflow_steps', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->foreignUuid('workflow_id')->constraint('workflows')->cascadeOnDelete();
+            $table->enum('type', ['trigger', 'action', 'condition']);
+            $table->jsonb('settings');
+            $table->integer('order')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workflow_steps');
+    }
+};
---
+++ src/database/migrations/2026_02_24_132746_create_workflow_logs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workflow_logs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('workflow_id')->constraint('workflows')->cascadeOnDelete();
+            $table->enum('status', ['success', 'failed', 'running']);
+            $table->jsonb('payload');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workflow_logs');
+    }
+};
